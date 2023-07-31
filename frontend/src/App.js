567fbc67dfc328ec99e1ab7e11e913205213d8a5
Colin Quelle <cmq1999@yahoo.com>
2023-07-31 17:30:44 -0400

    Created React frontend. Installed Tailwind Css

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex'>
+      <div className='bg-red-200 h-screen p-5 pt-8 w-64'>Sidebar</div>
+
+      <div className='p-7'>
+        <h1 className='text-2xl font-semibold'>Home Page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
